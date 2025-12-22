070fc78e65f7db5f3ee84ac2bf84b848491ee22f
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-12-22 18:24:22 +0500

    CheckoutPage

---
+++ pages/CheckoutPage.js
@@ -0,0 +1,49 @@
+export class CheckoutPage {
+  constructor(page) {
+    this.page = page;
+    this.cvvLoc = page.locator(".form__cc [type='text']");
+    this.nameOnCardLoc = page.locator(".form__cc [type='text']");
+    this.verifyEmailLoc = page.locator(".user__name label[type='text']");
+    this.countryLoc = page.locator("[placeholder*='Country']");
+    this.countriesSectionLoc = page.locator(".ta-results");
+    this.placeOrderBtnLoc = page.locator("text=Place Order");
+  }
+
+  async enterCVV(cvv) {
+    await this.cvvLoc.nth(1).fill(cvv);
+  }
+
+
+  async enterNameOnCard(nameOnCard) {
+    await this.nameOnCardLoc.nth(2).fill(nameOnCard);
+  }
+
+  async verifyEmail() {
+    await expect(this.verifyEmailLoc).toHaveText(email);
+  }
+
+  async selectCountry(country) {
+    await this.countryLoc.pressSequentially("Pa");
+
+    const optionsSection = await this.countriesSectionLoc();
+    await optionsSection.waitFor();
+
+    const optionsCount = await optionsSection.locator("button").count();
+    console.log(optionsCount);
+
+    for (let i = 0; i < optionsCount; i++) {
+      const optionText = await optionsSection.locator("button").nth(i).textContent();
+
+      if (optionText.trim() === country) {
+        optionsSection.locator("button").nth(i).click();
+        break;
+      }
+    }
+  }
+
+  async placeOrder(){
+
+      await this.placeOrderBtnLoc.click()
+
+  }
+}
--- tests/ClientApp_POM.spec.js
+++ tests/ClientApp_POM.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 import { Dashboard } from "../pages/Dashboard";
 import { CartPage } from "../pages/CartPage";
+import { CheckoutPage } from "../pages/CheckoutPage";
 
 
 
@@ -9,6 +10,7 @@ test.only("Browser Context Playwright Test", async ({ page }) => {
 
   const email = "abdurrehman@dummy.com";
   const name = "Abdul-ur-Rehman"
+  const cvv = "123"
   const password = "Dummy@123"
   const productName = "ADIDAS ORIGINAL";
 
@@ -30,32 +32,17 @@ test.only("Browser Context Playwright Test", async ({ page }) => {
   console.log(prodVisibility)
   expect(prodVisibility).toBeTruthy();
 
-  await cartPage.navigateToCheckout()  
+  await cartPage.navigateToCheckout()
 
-  await page.locator(".form__cc [type='text']").nth(1).fill("123");
-  await page.locator(".form__cc [type='text']").nth(2).fill("Abdul-ur-Rehman");
+  const checkoutPage = new CheckoutPage(page)
 
-  await expect(page.locator(".user__name label[type='text']")).toHaveText(email);
+  await checkoutPage.enterCVV(cvv)
+  await checkoutPage.enterNameOnCard(name)
 
-  await page.locator("[placeholder*='Country']").pressSequentially("Pa");
+  await checkoutPage.verifyEmail()
 
-  const optionsSection = await page.locator(".ta-results");
-  await optionsSection.waitFor();
-
-  const optionsCount = await optionsSection.locator("button").count();
-  console.log(optionsCount);
-
-  for (let i=0; i<optionsCount; i++){
-
-    const optionText = await optionsSection.locator("button").nth(i).textContent()
-
-    if(optionText.trim() === "Pakistan"){
-        optionsSection.locator("button").nth(i).click()
-        break
-  }
-}
-
-await page.locator("text=Place Order").click()
+  await checkoutPage.selectCountry("Pakistan")
+  await checkoutPage.placeOrder()
 
 await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ")
 
